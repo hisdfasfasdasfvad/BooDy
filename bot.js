3140433961c11c27ba1684a4ed44fca948f84b5d
hisdfasfasdasfvad <46384271+hisdfasfasdasfvad@users.noreply.github.com>
2019-01-05 02:20:58 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,6 +6,45 @@ client.on('ready', () => {
   console.log('Codes By BooDy');
 });
 
+
+//====================================================================
+//command = kick
+
+client.on('message',async message => {
+ if (message.author.kick) return;
+ if (!message.content.startsWith(prefix)) return;
+
+ let command = message.content.split(" ")[0];
+ command = command.slice(prefix.length);
+
+ let args = message.content.split(" ").slice(1);
+
+ if (command == "kick") {
+              if(!message.channel.guild) return;
+        
+ if(!message.guild.member(message.author).hasPermission("KICK_MEMBERS")) return message.reply("You Don't Have KICK_MEMBERS Permission").then(msg => msg.delete(5000));
+ if(!message.guild.member(client.user).hasPermission("KICK_MEMBERS")) return message.reply("I Don't Have KICK_Members Permission");
+ let user = message.mentions.users.first();
+ let reason = message.content.split(" ").slice(2).join(" ");
+
+ if (message.mentions.users.size < 1) return message.reply("منشن شخص");
+ if(!reason) return message.reply ("اكتب سبب الطرد");
+ if (!message.guild.member(user)
+ .bannable) return message.reply("لايمكنني طرد شخص اعلى من رتبتي");
+
+ message.guild.member(user).kick(7, user);
+
+ const banembed = new Discord.RichEmbed()
+ .setAuthor('Kicked !', user.displayAvatarURL)
+ .setColor("RANDOM")
+ .setTimestamp()
+ .addField("User:",  `[ + ${user.tag} + ]`)
+ .addField("By:", `[  + ${message.author.tag} +  ]`)
+ .addField("Reason:", `[ + ${reason} +  ]`)
+message.guild.channels.find("name","logs").send({embed : banembed})
+}
+});
+
 //====================================================================
 //command = Rainbow
 
