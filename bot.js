cf72c3a6a1bdc34a683fc595c21d3d9b0e8d781e
hisdfasfasdasfvad <46384271+hisdfasfasdasfvad@users.noreply.github.com>
2019-01-19 17:24:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -220,38 +220,38 @@ return;
 });
 
 // ================================================================================
-const developers = ["531059183282421800"]
+const developers = ["531059183282421800","359023200371212298"]
 var adminprefix = '!'
 client.on('message', message => {
-    var argresult = message.content.split(`).slice(1).join(' ');
+    var argresult = message.content.split(``).slice(1).join(' ');
       if (!developers.includes(message.author.id)) return;
       
   if (message.content.startsWith(adminprefix + 'setg')) {
     client.user.setGame(argresult);
-      message.channel.send(:white_check_mark:   ${argresult})
+      message.channel.send(`:white_check_mark:   ${argresult}`)
   } else 
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'setw')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(:white_check_mark:   ${argresult})
+      message.channel.send(`:white_check_mark:   ${argresult}`)
   } else 
   if (message.content.startsWith(adminprefix + 'setl')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(:white_check_mark:   ${argresult})
+      message.channel.send(`:white_check_mark:   ${argresult}`)
   } else 
   if (message.content.startsWith(adminprefix + 'sets')) {
     client.user.setGame(argresult, "https://www.twitch.tv/dream");
-      message.channel.send(:white_check_mark:)
+      message.channel.send(`:white_check_mark:`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
-      message.channel.send(Changing The Name To ..${argresult} )
+      message.channel.send(`Changing The Name To ..${argresult} `)
 } else
 if (message.content.startsWith(adminprefix + 'setava')) {
   client.user.setAvatar(argresult);
-    message.channel.send(Changing The Avatar To :${argresult} `);
+    message.channel.send(`Changing The Avatar To :${argresult} `);
 }
 });
 
